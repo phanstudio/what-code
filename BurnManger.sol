d21d55e7ee6b8ee164e32a302a9b7aad3ec3ade5
phanstudio <donenoelle@gmail.com>
2025-08-11 10:42:13 +0100

    update

--- BurnManger.sol
+++ BurnManger.sol
@@ -1,6 +1,6 @@
 // contracts/BurnManager.sol
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.28;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
@@ -21,14 +21,13 @@ contract BurnManager is Ownable, ReentrancyGuard, Pausable {
     using Address for address payable;
 
     address public immutable nftContract;
-    uint256 private burnFee = 0.0000001 ether;
+    uint256 private burnFee = 0.1 gwei;
     uint16 public burnAmount = 10;
     
     mapping(uint32 => bool) private isBurned;
     mapping(uint32 => bool) private isUpdated;
     
-    // Maximum fee limit to prevent owner abuse
-    uint256 private constant MAX_BURN_FEE = 0.1 ether;
+    uint256 private constant MAX_BURN_FEE = 1 ether;
 
     event TokensBurned(address indexed burner, uint32[] tokenIds, uint256 totalFee, uint32 indexed updateId);
     event BurnFeeUpdated(uint256 oldFee, uint256 newFee);
@@ -53,137 +52,58 @@ contract BurnManager is Ownable, ReentrancyGuard, Pausable {
         nftContract = _nftContract;
     }
 
-    // function createPremium(uint32[] calldata tokenIds, uint32 update_id)
-    //     external
-    //     payable
-    //     nonReentrant
-    //     whenNotPaused
-    // {
-    //     uint256 totalFee = burnFee;
-    //     if (msg.value < totalFee) revert InsufficientPayment();
-    //     if (tokenIds.length != burnAmount) revert("Burn the required token amount");
-
-    //     IERC721Burnable nft = IERC721Burnable(nftContract);
-        
-    //     // Validate all tokens first
-    //     for (uint256 i = 0; i < tokenIds.length; i++) {
-    //         uint32 tokenId = tokenIds[i];
-    //         if (tokenId == update_id) revert("update_id cannot be among tokenIds");
-    //         if (isBurned[tokenId]) revert TokenAlreadyBurned();
-    //         if (nft.ownerOf(tokenId) != msg.sender) revert NotTokenOwner();
-    //     }
-
-    //     // Handle refund before state changes
-    //     uint256 excess = msg.value - totalFee;
-    //     if (excess > 0) {
-    //         (bool success, ) = payable(msg.sender).call{value: excess}("");
-    //         if (!success) revert("Refund failed");
-    //     }
-
-    //     // Now make state changes
-    //     isUpdated[update_id] = true;
-    //     for (uint32 i = 0; i < tokenIds.length; i++) {
-    //         uint32 tokenId = tokenIds[i];
-    //         isBurned[tokenId] = true;
-    //         nft.burn(tokenId);
-    //     }
-    //     emit TokensBurned(msg.sender, tokenIds, totalFee, update_id);
-    // }
-
-    // function createPremium(uint32[] calldata tokenIds, uint32 update_id)
-    //     external
-    //     payable
-    //     nonReentrant
-    //     whenNotPaused
-    // {
-    //     uint32 tokenCount = uint32(tokenIds.length);
-    //     if (tokenCount != burnAmount) revert("Burn the required token amount");
-    //     if (tokenCount == 0) revert("No tokens provided");
-        
-    //     uint256 totalFee = burnFee;
-    //     if (msg.value < totalFee) revert InsufficientPayment();
-
-    //     IERC721Burnable nft = IERC721Burnable(nftContract);
-        
-    //     // Validate update_id first
-    //     if (nft.ownerOf(update_id) != msg.sender) revert NotTokenOwner();
-    //     if (isBurned[update_id]) revert TokenAlreadyBurned();
-    //     if (isUpdated[update_id]) revert("Token already updated");
-        
-    //     // Validate all tokens in a single loop
-    //     for (uint32 i = 0; i < tokenCount; i++) {
-    //         uint32 tokenId = tokenIds[i];
-    //         if (tokenId == update_id) revert("update_id cannot be among tokenIds");
-    //         if (isBurned[tokenId]) revert TokenAlreadyBurned();
-    //         if (nft.ownerOf(tokenId) != msg.sender) revert NotTokenOwner();
-    //     }
-
-    //     // STATE CHANGES (Effects)
-    //     isUpdated[update_id] = true;
-    //     for (uint32 i = 0; i < tokenCount; i++) {
-    //         uint32 tokenId = tokenIds[i];
-    //         isBurned[tokenId] = true;
-    //         nft.burn(tokenId);
-    //     }
-        
-    //     // EXTERNAL INTERACTIONS (Interactions)
-    //     emit TokensBurned(msg.sender, tokenIds, totalFee, update_id);
-        
-    //     // Handle refund last
-    //     uint256 excess = msg.value - totalFee;
-    //     if (excess > 0) {
-    //         (bool success, ) = payable(msg.sender).call{value: excess}("");
-    //         if (!success) revert("Refund failed");
-    //     }
-    // }
-
-    function createPremium(uint32[] calldata tokenIds, uint32 update_id)
+    function createPremium(uint32[] calldata tokenIds, uint32 updateId)
         external
         payable
         nonReentrant
         whenNotPaused
     {
+        _validateInput(tokenIds, updateId);
+        _processBurn(tokenIds, updateId);
+        _refundExcess();
+    }
+
+    function _validateInput(uint32[] calldata tokenIds, uint32 updateId) private view {
         uint32 tokenCount = uint32(tokenIds.length);
         if (tokenCount == 0) revert("No tokens provided");
         if (tokenCount != burnAmount) revert("Burn the required token amount");
 
-        uint256 totalFee = burnFee;
-        if (msg.value < totalFee) revert("Insufficient payment");
+        if (msg.value < burnFee) revert("Insufficient payment");
 
         IERC721Burnable nft = IERC721Burnable(nftContract);
 
-        // Validate update_id first
-        if (nft.ownerOf(update_id) != msg.sender) revert("Not token owner");
-        if (isBurned[update_id]) revert("Token already burned");
-        if (isUpdated[update_id]) revert("Token already updated");
+        if (nft.ownerOf(updateId) != msg.sender) revert("Not token owner");
+        if (isBurned[updateId]) revert("Token already burned");
+        if (isUpdated[updateId]) revert("Token already updated");
 
-        // Validate all tokens in a single loop
         for (uint32 i = 0; i < tokenCount; i++) {
             uint32 tokenId = tokenIds[i];
-            if (tokenId == update_id) revert("update_id cannot be among tokenIds");
+            if (tokenId == updateId) revert("updateId cannot be among tokenIds");
             if (isBurned[tokenId]) revert("Token already burned");
             if (nft.ownerOf(tokenId) != msg.sender) revert("Not token owner");
         }
+    }
 
-        // STATE CHANGES (Effects)
-        isUpdated[update_id] = true;
-        for (uint32 i = 0; i < tokenCount; i++) {
+    function _processBurn(uint32[] calldata tokenIds, uint32 updateId) private {
+        IERC721Burnable nft = IERC721Burnable(nftContract);
+
+        isUpdated[updateId] = true;
+        for (uint32 i = 0; i < tokenIds.length; i++) {
             uint32 tokenId = tokenIds[i];
             isBurned[tokenId] = true;
             nft.burn(tokenId);
         }
 
-        // EXTERNAL INTERACTIONS (Interactions)
-        emit TokensBurned(msg.sender, tokenIds, totalFee, update_id);
+        emit TokensBurned(msg.sender, tokenIds, burnFee, updateId);
+    }
 
-        // Handle refund last
-        uint256 excess = msg.value - totalFee;
+    function _refundExcess() private {
+        uint256 excess = msg.value - burnFee;
         if (excess > 0) {
             (bool success, ) = payable(msg.sender).call{value: excess}("");
             if (!success) revert("Refund failed");
         }
     }
-
 
     function setBurnAmount(uint16 amount) external onlyOwner {
         require(amount > 0, "Minimum must be > 0");
@@ -216,19 +136,19 @@ contract BurnManager is Ownable, ReentrancyGuard, Pausable {
         emit BurnFeeUpdated(oldFee, _newFee);
     }
 
-    function withdrawFunds() external onlyOwner nonReentrant {
-        uint256 balance = address(this).balance;
-        if (balance == 0) revert NoFundsToWithdraw();
-        
-        payable(owner()).sendValue(balance);
-        emit FundsWithdrawn(owner(), balance);
-    }
-
-    function withdrawPartialFunds(uint256 _amount) external onlyOwner nonReentrant {
+    function _withdrawFunds(uint256 _amount) private nonReentrant {
         if (_amount == 0 || _amount > address(this).balance) revert NoFundsToWithdraw();
-        
+
         payable(owner()).sendValue(_amount);
         emit FundsWithdrawn(owner(), _amount);
+    }
+
+    function withdrawFunds() external onlyOwner {
+        _withdrawFunds(address(this).balance);
+    }
+
+    function withdrawPartialFunds(uint256 _amount) external onlyOwner {
+        _withdrawFunds(_amount);
     }
 
     function pause() external onlyOwner {
